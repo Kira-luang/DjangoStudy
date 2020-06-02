9b6fa10a45bf28b8ace3cb1986be9affbe7938d2
kira <myemail>
2020-06-03 02:38:23 +0800

    Django的csrf源代码查看并了解基本原理

--- study/App/views.py
+++ study/App/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def hello(request):
+    if request.method == 'GET':
+        return render(request , 'hello.html')
+    elif request.method == 'POST':
+        name = request.POST.get('name')
+        return HttpResponse('Hello {}'.format(name))
